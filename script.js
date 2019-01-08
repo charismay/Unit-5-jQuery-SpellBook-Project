79f5a36f44737a7421b78198415b8ebf4cdebc3d
charismay <43378956+charismay@users.noreply.github.com>
2019-01-08 09:47:55 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+//Spell #1
+
+$("#makeup ").click(function(){
+$("#palette").toggle();
+ 
+});
+
+
+
+//Spell #2
+
+$("#youtube").click(function(){
+$("#sisters").fadeOut();
+ 
+});
+
+
+
+//Spell #3
+
+$("#money").click(function(){
+$("#networth").hide();
+});
+
+
+
